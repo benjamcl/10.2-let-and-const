407b7c98ecf70de8f6d9c2c1a7437343ea09bc43
benjamcl <bencliu92@gmail.com>
2021-05-23 15:49:27 -0700

    Homework

---
+++ app.js
@@ -0,0 +1,26 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+//ES2015 Version
+
+const PI2 = 3.14
+PI2 = 42;
+/*
+
+What is the difference between var and let?
+
+You can redeclare with var, but not with let, but you can change the value of let. 
+
+What is the difference between var and const?
+
+You cannot redeclare or change the value of const compared to var.
+
+What is the difference between let and const?
+
+You can change the value with let, but not with const.
+
+What is hoisting?
+
+Hoising is referencing a variable that is available globally. 
+
+*/
